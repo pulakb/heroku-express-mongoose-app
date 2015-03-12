e1d092bd73c79940f59a7504f39a639ea634f9f3
pulakb <bhattacharyyapulak@gmail.com>
2015-03-12 18:43:07 +0530

    DB URI Added

--- controllers/api.js
+++ controllers/api.js
@@ -9,9 +9,9 @@
 var Thread = require('../models/thread.js');
 var Post = require('../models/post.js');
 
-exports.post = function(req, res) {
+/*exports.post = function(req, res) {
     new Thread({title: req.body.title, author: req.body.author}).save();
-}
+}*/
 
 exports.list = function(req, res) {
   Thread.find(function(err, threads) {
@@ -20,10 +20,11 @@ exports.list = function(req, res) {
 }
 
 // first locates a thread by title, then locates the replies by thread ID.
+/*
 exports.show = (function(req, res) {
     Thread.findOne({title: req.params.title}, function(error, thread) {
         var posts = Post.find({thread: thread._id}, function(error, posts) {
           res.send([{thread: thread, posts: posts}]);
         });
     })
-});
+});*/
--- server.js
+++ server.js
@@ -37,13 +37,11 @@ var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
 // configuration ===============================================================
-mongoose.connect(database.url); 	// connect to mongoDB database on modulus.io
+mongoose.connect(database.url); 	// connect to mongoDB database on MongoLab
 
 app.use(morgan('dev')); // log every request to the console
 app.use(bodyParser.urlencoded({'extended':'true'})); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
-app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
 
 
 // routes ======================================================================
