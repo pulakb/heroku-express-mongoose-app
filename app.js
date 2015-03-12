7d823706d711f0211939ff8b3b687c721357a845
pulakb <bhattacharyyapulak@gmail.com>
2015-03-12 17:04:45 +0530

    Folder Structure

---
+++ app.js
@@ -0,0 +1,55 @@
+/*
+// The main application script, ties everything together.
+
+var express = require('express');
+var mongoose = require('mongoose');
+var app = module.exports = express.createServer();
+
+// connect to Mongo when the app initializes
+mongoose.connect('mongodb://localhost/norum');
+
+app.configure(function(){
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+});
+
+// set up the RESTful API, handler methods are defined in api.js
+var api = require('./controllers/api.js');
+app.post('/thread', api.post);
+app.get('/thread/:title.:format?', api.show);
+app.get('/thread', api.list);
+
+app.listen(3000);
+console.log("Express server listening on port %d", app.address().port);
+*/
+
+
+
+
+var express  = require('express');
+var app      = express(); 								// create our app w/ express
+var mongoose = require('mongoose'); 					// mongoose for mongodb
+var port  	 = process.env.PORT || 8080; 				// set the port
+var database = require('./config/database'); 			// load the database config
+var morgan   = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+// configuration ===============================================================
+mongoose.connect(database.url); 	// connect to mongoDB database on modulus.io
+
+app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
+app.use(morgan('dev')); // log every request to the console
+app.use(bodyParser.urlencoded({'extended':'true'})); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
+
+
+// routes ======================================================================
+require('./app/routes.js')(app);
+
+// listen (start app with node server.js) ======================================
+app.listen(port);
+console.log("App listening on port " + port);
