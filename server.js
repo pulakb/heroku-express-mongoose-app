82a1772e6fc8952ee28d911c14155792aa912ec4
pulakb <bhattacharyyapulak@gmail.com>
2015-03-12 18:56:49 +0530

    Comments removed

--- server.js
+++ server.js
@@ -1,32 +1,6 @@
 /*
-// The main application script, ties everything together.
-
-var express = require('express');
-var mongoose = require('mongoose');
-var app = module.exports = express.createServer();
-
-// connect to Mongo when the app initializes
-mongoose.connect('mongodb://localhost/norum');
-
-app.configure(function(){
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-});
-
-// set up the RESTful API, handler methods are defined in api.js
-var api = require('./controllers/api.js');
-app.post('/thread', api.post);
-app.get('/thread/:title.:format?', api.show);
-app.get('/thread', api.list);
-
-app.listen(3000);
-console.log("Express server listening on port %d", app.address().port);
-*/
-
-
-
-
+* Server file
+* */
 var express  = require('express');
 var app      = express(); 								// create our app w/ express
 var mongoose = require('mongoose'); 					// mongoose for mongodb
